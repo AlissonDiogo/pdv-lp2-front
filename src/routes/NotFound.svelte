a8f8140701ec96fc1534c6fca00cbed44c8291d8
Isaac Lourenço <isaacmarlondasilvalourenco@gmail.com>
2022-11-10 10:01:33 -0300

    feat: NotFound page

---
+++ src/routes/NotFound.svelte
@@ -0,0 +1,12 @@
+<script>
+    import { pop } from "svelte-spa-router";
+
+    const goRescuePage = () => {
+        pop();
+    }
+</script>
+
+<main>
+    <h1>Sorry, you are lost :(</h1>
+    <button on:click={goRescuePage}>Rescue me!</button>
+</main>
