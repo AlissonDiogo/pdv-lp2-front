49cf146ca14482489636cf94eea8b64f2a009d9b
Isaac Lourenço <isaacmarlondasilvalourenco@gmail.com>
2022-11-14 17:23:12 -0300

    feat: Waiting with own style

--- src/routes/Waiting.svelte
+++ src/routes/Waiting.svelte
@@ -1,18 +1,35 @@
 <script>
-    const months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
-    let now = new Date();
-  
-    const halfSecondMs = 30000;
-    setInterval(() => {
-      now = new Date();
-    }, halfSecondMs);
-  
-  </script>
-  
-  <main>
-    <h1>FREE CASHIER</h1>
-    <section>
-      <h2>{now.getHours()}:{now.getMinutes()}</h2>
-      <p>{now.getDay()} {months[now.getMonth()].toUpperCase()} {now.getFullYear()}</p>
-    </section>
-  </main>
+  const months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+  let now = new Date();
+
+  const halfSecondMs = 30000;
+  setInterval(() => {
+    now = new Date();
+  }, halfSecondMs);
+
+</script>
+
+<main>
+  <h1>FREE CASHIER</h1>
+  <section>
+    <h2>{now.getHours()}:{now.getMinutes()}</h2>
+    <p>{now.getDay()} {months[now.getMonth()].toUpperCase()} {now.getFullYear()}</p>
+  </section>
+</main>
+
+<style>
+  main {
+    height: 100vh;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    gap: 1rem;
+  }
+
+  section {
+    display: grid;
+    gap: 1rem;
+  }
+</style>
