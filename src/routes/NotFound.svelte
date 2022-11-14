b062f0a96b7f2d320454b6d10101308b574f44b3
Isaac Lourenço <isaacmarlondasilvalourenco@gmail.com>
2022-11-14 17:23:24 -0300

    feat: NotFound with own style

--- src/routes/NotFound.svelte
+++ src/routes/NotFound.svelte
@@ -10,3 +10,12 @@
     <h1>Sorry, you are lost :(</h1>
     <button on:click={goRescuePage}>Rescue me!</button>
 </main>
+
+<style>
+    main {
+        display: grid;
+        place-content: center;
+        height: 100vh;
+        gap: 1rem;
+    }
+</style>
