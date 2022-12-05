a14aa2a98e0d33ff34f58f8e482ba02360950578
Isaac Lourenço <isaacmarlondasilvalourenco@gmail.com>
2022-12-05 17:33:55 -0300

    feat: Login page with validation

--- src/App.svelte
+++ src/App.svelte
@@ -3,10 +3,12 @@
   import NotFound from "./routes/NotFound.svelte";
   import Waiting from "./routes/Waiting.svelte";
   import Sell from "./routes/Sell.svelte";
+  import SignIn from "./routes/SignIn.svelte";
 
   const routes = {
     "/": Waiting,
     "/sell": Sell,
+    "/sign-in": SignIn,
     "*": NotFound,
   };
 </script>
---
+++ src/routes/SignIn.svelte
@@ -0,0 +1,63 @@
+<script>
+	import { useForm, validators, HintGroup, Hint, email, required } from "svelte-use-form";
+	const form = useForm();
+</script>
+
+<main>
+  <form use:form>
+    <h1>Welcome!</h1>
+
+    <section>
+      <input
+        placeholder="Your email"
+        type="email"
+        name="email" use:validators={[required, email]}
+      />
+      <HintGroup for="email">
+        <Hint on="required">This is a mandatory field</Hint>
+        <Hint on="email" hideWhenRequired>Email is not valid</Hint>
+      </HintGroup>
+    </section>
+
+    <section>
+      <input
+        placeholder="Your password"
+        type="password"
+        name="password"
+        use:validators={[required]}
+      />
+      <Hint for="password" on="required">This is a mandatory field</Hint>
+    </section>
+
+    <button disabled={!$form.valid}>Sign in</button>
+  </form>
+</main>
+
+<style>
+	:global(.touched:invalid) {
+    border: 1px solid red;
+	}
+
+  main {
+    height: 100vh;
+    display: grid;
+    place-content: center;
+  }
+
+  form {
+    display: grid;
+    gap: 2rem;
+  }
+
+  form input,
+  form button {
+    padding: 1rem;
+    font-size: 1rem;
+    border-radius: 8px;
+  }
+
+  form section {
+    display: grid;
+    gap: 10px;
+  }
+</style>
